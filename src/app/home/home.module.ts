df861fbb7a559d271198af0c662a84e20712370b
German Fica <germafica@gmail.com>
2022-05-17 17:26:55 -0300

    Add hero header component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { HeroHeaderComponent } from './components/hero-header/hero-header.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeroHeaderComponent
   ],
   imports: [
     CommonModule,
