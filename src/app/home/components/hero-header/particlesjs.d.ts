3ef1965c8bd28473386b71776c12d57f47889c39
German Fica <germafica@gmail.com>
2022-05-18 15:48:40 -0300

    Add particlesjs declaration

---
+++ src/app/home/components/hero-header/particlesjs.d.ts
@@ -0,0 +1,11 @@
+// declare var particlesJS: (
+//     arg0: string,
+//     arg1: {
+//         particles: {
+//             number: any;
+//             color: {
+//                 value: string
+//             };
+//         };
+//     }) => void;
+declare var particlesJS: (tagId: string, params: any) => void;
