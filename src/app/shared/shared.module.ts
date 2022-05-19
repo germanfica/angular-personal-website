796916629cb800ba389f5e92b3eee5d99e930e28
German Fica <germafica@gmail.com>
2022-05-18 22:13:18 -0300

    Add rounded-button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RoundedButtonComponent } from './components/rounded-button/rounded-button.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    RoundedButtonComponent
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    RoundedButtonComponent
   ],
   imports: [
     CommonModule
