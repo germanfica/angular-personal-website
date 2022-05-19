895f2c92d342d12a9ec5af9778304c9fec0bd045
German Fica <germafica@gmail.com>
2022-05-18 22:29:52 -0300

    Update rounded-button style

--- src/app/shared/components/rounded-button/rounded-button.component.ts
+++ src/app/shared/components/rounded-button/rounded-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rounded-button',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rounded-button.component.scss']
 })
 export class RoundedButtonComponent implements OnInit {
+  @Input() text: String;
 
-  constructor() { }
+  constructor() {
+    this.text = "button";
+  }
 
   ngOnInit(): void {
   }
