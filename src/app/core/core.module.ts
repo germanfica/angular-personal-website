67aca23405b790f24b9f14e99f2c3a0e588fc122
German Fica <germafica@gmail.com>
2022-09-12 20:29:10 -0300

    Add contact service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactService } from './services/contact.service';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    ContactService
   ]
 })
 export class CoreModule { }
