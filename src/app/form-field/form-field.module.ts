1ef86187f2ab39425147402018db7c9e33ac91d9
German Fica <germafica@gmail.com>
2022-09-21 13:17:04 -0300

    Add error directive

--- src/app/form-field/form-field.module.ts
+++ src/app/form-field/form-field.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { InputFieldComponent } from './components/input-field/input-field.component';
 import { FormsModule } from '@angular/forms';
 import { HintDirective } from './directives/hint.directive';
+import { ErrorDirective } from './directives/error.directive';
 
 @NgModule({
   declarations: [
     InputFieldComponent,
     HintDirective,
+    ErrorDirective,
   ],
   exports: [
     InputFieldComponent
