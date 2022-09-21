a28860aca183cefa447fabaab58a94f66c167547
German Fica <germafica@gmail.com>
2022-09-21 13:19:49 -0300

    Add text content in hint directive

--- src/app/form-field/directives/hint.directive.ts
+++ src/app/form-field/directives/hint.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHint]'
 })
-export class HintDirective {
+export class HintDirective implements OnInit {
+  textContent: string = '';
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  ngOnInit(): void {
+    this.textContent = this.el.nativeElement.textContent;
+  }
 }
