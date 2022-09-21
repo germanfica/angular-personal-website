889522c8dcdeb7173397f9ceced5b0d70984e758
German Fica <germafica@gmail.com>
2022-09-21 13:16:15 -0300

    Add hint directive

--- src/app/form-field/form-field.module.ts
+++ src/app/form-field/form-field.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputFieldComponent } from './components/input-field/input-field.component';
 import { FormsModule } from '@angular/forms';
+import { HintDirective } from './directives/hint.directive';
 
 @NgModule({
   declarations: [
     InputFieldComponent,
+    HintDirective,
   ],
   exports: [
     InputFieldComponent
