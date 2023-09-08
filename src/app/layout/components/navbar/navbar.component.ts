67c1e1869057f8acadfd11c29fa47324fdfa20dc
German Fica <germafica@gmail.com>
2023-09-08 13:32:15 -0300

    Add scroll lock feature when navbar menu is active
    - Imported HostListener to listen for DOM events.
    - Added a `scrollHandler` function to prevent window scrolling when the menu is active.
    - Updated `toggleMenu` method to lock or restore body scroll depending on the state of `menuActive`.
    
    The changes introduce a feature that locks the page scroll when the navigation menu is open, thereby enhancing the user experience.

--- src/app/layout/components/navbar/navbar.component.ts
+++ src/app/layout/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+// Import HostListener to listen for DOM events
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -15,8 +16,24 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // Lock scrolling when the menu is open
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event: any) {
+    if (this.menuActive) {
+      window.scrollTo(0, 0);
+    }
+  }
+
   toggleMenu(): void {
     console.log("toggleMenu!");
     this.menuActive = !this.menuActive;
+
+    if (this.menuActive) {
+      // Lock body scroll
+      document.body.style.overflow = 'hidden';
+    } else {
+      // Restore body scroll
+      document.body.style.overflow = 'auto';
+    }
   }
 }
