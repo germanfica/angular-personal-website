810d6b7bef8bf56ed39765ec72fa5891fe7088ad
German Fica <germafica@gmail.com>
2023-09-08 19:08:58 -0300

    Refactor PageNotFoundComponent to use rounded button and update navigation
    - Replace standard button with app-rounded-button in page-not-found.component.html
    - Update navigation logic to redirect to '/home' instead of '/'
    - Import ButtonModule in page-not-found.module.ts

--- src/app/page-not-found/page-not-found.module.ts
+++ src/app/page-not-found/page-not-found.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PageNotFoundRoutingModule } from './page-not-found-routing.module';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ButtonModule } from '@app/button/button.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   ],
   imports: [
     CommonModule,
-    PageNotFoundRoutingModule
+    PageNotFoundRoutingModule,
+    ButtonModule
   ]
 })
 export class PageNotFoundModule { }
