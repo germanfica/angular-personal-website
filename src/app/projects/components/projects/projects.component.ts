677c59c042f215a3d22abe709c17eeb8a2fcbc91
German Fica <germafica@gmail.com>
2024-03-28 23:23:29 -0300

    Add SEO in projects component

--- src/app/projects/components/projects/projects.component.ts
+++ src/app/projects/components/projects/projects.component.ts
@@ -3,6 +3,9 @@ import { ProjectsService } from '@core/services/projects.service';
 import { Project } from '@core/models/project';
 import { NavbarService } from '@app/layout/services/navbar.service';
 import { Subscription } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { UrlService } from '@core/services/url.service';
 
 @Component({
   selector: 'app-projects',
@@ -13,10 +16,51 @@ export class ProjectsComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription(); // Matener un registro de las suscripciones
   projects: Project[] = [];
 
-  constructor(private projectsService: ProjectsService, private navbarService: NavbarService) { }
+  constructor(
+    private projectsService: ProjectsService,
+    private navbarService: NavbarService,
+    private titleService: Title,
+    private metaService: Meta,
+    private router: Router,
+    private urlService: UrlService
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('Projects - German Fica');
+
+    // Open Graph Meta Tags
+    this.metaService.updateTag({ property: 'og:title', content: 'Projects - German Fica' });
+    this.metaService.updateTag({ property: 'og:description', content: 'See all my thinking process' });
+    //this.metaService.updateTag({ property: 'og:image', content: 'Preview image URL' });
+    this.metaService.updateTag({ property: 'og:url', content: this.urlService.getBaseUrl() + this.router.url });
+    this.metaService.updateTag({ property: 'og:site_name', content: 'German Fica' });
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:locale', content: 'en_US' });
+
+    // Twitter Card Meta Tags
+    this.metaService.updateTag({ name: 'twitter:card', content: 'See all my thinking process' });
+    this.metaService.updateTag({ name: 'twitter:site', content: '@germanfica' });
+    this.metaService.updateTag({ name: 'twitter:title', content: 'Projects - German Fica' });
+    this.metaService.updateTag({ name: 'twitter:description', content: 'See all my thinking process' });
+    //this.metaService.updateTag({ name: 'twitter:image', content: 'Preview image URL' });
+    this.metaService.updateTag({ name: 'twitter:creator', content: '@germanfica' });
+
+    // Update navbar style
     this.navbarService.updateNavbarState({ isSticky: true, navbarStyle: 'colored' });
+
+    // Fetch projects
+    this.fetchProjects();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();  // Desuscribe todas las suscripciones
+  }
+
+  doSomething(projectId: number): void {
+    console.log(`doSomething!!!!! ${projectId}`);
+  }
+
+  private fetchProjects() {
     const projServSub = this.projectsService.getAllProjects().subscribe(data => {
       this.projects = data;
 
@@ -37,13 +81,5 @@ export class ProjectsComponent implements OnInit, OnDestroy {
       console.log('Tipo de data:', typeof hola);
     });
     this.subscription.add(projServSub);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();  // Desuscribe todas las suscripciones
-  }
-
-  doSomething(projectId: number): void {
-    console.log(`doSomething!!!!! ${projectId}`);
   }
 }
