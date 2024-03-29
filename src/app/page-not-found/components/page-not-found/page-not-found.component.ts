1b5b304cfa4935ce4338b6d33ed60790fef68a93
German Fica <germafica@gmail.com>
2024-03-29 00:25:48 -0300

    Update meta tags in page not found component

--- src/app/page-not-found/components/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { NavbarService } from '@app/layout/services/navbar.service';
 
@@ -9,13 +10,45 @@ import { NavbarService } from '@app/layout/services/navbar.service';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor(private router: Router, private navbarService: NavbarService) { }
+  constructor(
+    private router: Router,
+    private navbarService: NavbarService,
+    private titleService: Title,
+    private metaService: Meta
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('404 not found');
+
+    // Clean existing metatags
+    this.clearMetaTags();
+
+    // Ignore
+    this.metaService.updateTag({ name: 'robots', content: 'noindex' });
+    // Update navbar style
     this.navbarService.updateNavbarState({ isSticky: false, navbarStyle: 'colored-pos-abs' });
   }
 
   back(): void {
     this.router.navigate(['/']);
+  }
+
+  private clearMetaTags(): void {
+    // Eliminar metaetiquetas de Open Graph
+    this.metaService.removeTag("property='og:title'");
+    this.metaService.removeTag("property='og:description'");
+    this.metaService.removeTag("property='og:image'");
+    this.metaService.removeTag("property='og:url'");
+    this.metaService.removeTag("property='og:site_name'");
+    this.metaService.removeTag("property='og:type'");
+    this.metaService.removeTag("property='og:locale'");
+
+    // Eliminar metaetiquetas de Twitter Card
+    this.metaService.removeTag("name='twitter:card'");
+    this.metaService.removeTag("name='twitter:site'");
+    this.metaService.removeTag("name='twitter:title'");
+    this.metaService.removeTag("name='twitter:description'");
+    this.metaService.removeTag("name='twitter:image'");
+    this.metaService.removeTag("name='twitter:creator'");
   }
 }
