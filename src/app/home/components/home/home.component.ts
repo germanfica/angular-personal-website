1d4f69ee7fb57da4fcbd2fec80de0633d41de6de
German Fica <germafica@gmail.com>
2024-03-28 23:43:34 -0300

    Add SEO in home component

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { NavbarService } from '@app/layout/services/navbar.service';
+import { UrlService } from '@core/services/url.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +11,35 @@ import { NavbarService } from '@app/layout/services/navbar.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private navbarService: NavbarService) { }
+  constructor(
+    private navbarService: NavbarService,
+    private titleService: Title,
+    private metaService: Meta,
+    private urlService: UrlService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('German Fica');
+
+    // Open Graph Meta Tags
+    this.metaService.updateTag({ property: 'og:title', content: 'German Fica' });
+    this.metaService.updateTag({ property: 'og:description', content: 'Software Developer | Web Developer | Unity Developer' });
+    this.metaService.updateTag({ property: 'og:image', content: `${this.urlService.getBaseUrl()}/assets/images/home_preview_thumbnail.png` });
+    this.metaService.updateTag({ property: 'og:url', content: this.urlService.getBaseUrl() + this.router.url });
+    this.metaService.updateTag({ property: 'og:site_name', content: 'German Fica' });
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:locale', content: 'en_US' });
+
+    // Twitter Card Meta Tags
+    this.metaService.updateTag({ name: 'twitter:card', content: 'Software Developer | Web Developer | Unity Developer' });
+    this.metaService.updateTag({ name: 'twitter:site', content: '@germanfica' });
+    this.metaService.updateTag({ name: 'twitter:title', content: 'German Fica' });
+    this.metaService.updateTag({ name: 'twitter:description', content: 'Software Developer | Web Developer | Unity Developer' });
+    this.metaService.updateTag({ name: 'twitter:image', content: `${this.urlService.getBaseUrl()}/assets/images/home_preview_thumbnail.png` });
+    this.metaService.updateTag({ name: 'twitter:creator', content: '@germanfica' });
+
+    // Update navbar style
     this.navbarService.updateNavbarState({ isSticky: false, navbarStyle: 'transparent' });
   }
 
