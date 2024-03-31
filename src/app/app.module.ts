5c67975c3c039279543bcf022c7a2b6aa17b8010
German Fica <germafica@gmail.com>
2024-03-31 01:17:36 -0300

    Enable caching data when using HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     LayoutModule
   ],
   providers: [
-    provideClientHydration(),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      }),
+    ),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),  // Habilitar fetch para HttpClient,
   ],
