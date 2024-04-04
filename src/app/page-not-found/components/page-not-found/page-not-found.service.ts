0d1250171cb324c962823f6cba7954b9f8225bc0
German Fica <germafica@gmail.com>
2024-04-03 22:40:33 -0300

    Delete logs in page-not-found.service

--- src/app/page-not-found/components/page-not-found/page-not-found.service.ts
+++ src/app/page-not-found/components/page-not-found/page-not-found.service.ts
@@ -1,4 +1,4 @@
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { isPlatformServer } from '@angular/common';
 import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
 import { RESPONSE } from 'src/express.token';
 import { Response } from 'express';
@@ -15,7 +15,6 @@ import { Response } from 'express';
 })
 export class PageNotFoundService {
   private isServer: boolean;
-  private isBrowser: boolean;
 
   /**
    * @param {Object} platformId - A token that indicates the platform the application is running on.
@@ -23,30 +22,14 @@ export class PageNotFoundService {
    */
   constructor(@Inject(PLATFORM_ID) private platformId: Object, @Optional() @Inject(RESPONSE) private response: Response) {
     this.isServer = isPlatformServer(platformId);
-    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   /**
     * Sets the HTTP status code to 404 (Not Found) if the service is executed on the server side.
     *
-    * Logs a warning if the method is mistakenly called on the client side, as 404 status should
-    * only be set server-side to inform clients about non-existent resources.
     */
   setNotFoundStatusCode(): void {
-    if (this.isBrowser && !this.isServer) {
-      console.error(
-        `Oops! It seems like you're trying to navigate to a non-existent page from the client-side. ` +
-        `In Angular SSR, certain actions like setting a 404 status should be performed on the server-side. ` +
-        `When using \`setNotFoundStatus()\`, please ensure that you are not redirecting to non-existent pages with \`@angular/router\`.`
-      );
-      console.warn(
-        `Hint: Try refreshing the page to retrieve any server-side changes, including 404 status code updates.`
-      );
-    }
-
     if (this.isServer && this.response) {
-      console.log("Great! This is the server side view. Sending 404 status code...")
-      console.error("404 not found")
       this.response.status(404);
     }
   }
